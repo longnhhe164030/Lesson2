f97c1673d642b46fa6d94bf7c379a53749d52dd6
Nguyễn Hoàng Long <longnhhe164030@fpt.edu.vn>
2024-11-22 17:18:36 +0700

    Initial commit

---
+++ UserRegistration/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    User registration
+    </head>
+    <body>
+    <label for="name">User Name:</label>
+        <input type="text" name="name"><br>
+        <label for="email">Email:</label>
+        <input type="text" name="email"><br>
+        <label for="phone">Phone:</label>
+        <input type="number" name="phone"><br>
+
+        <button type="submit" name="action" value="Submit">Submit</button>
+    </body>
+</html>
